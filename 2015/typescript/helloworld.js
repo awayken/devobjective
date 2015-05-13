a1a1c93084c475ee46420366c7b7a93a911a4815
Miles Rausch <miles@milesrausch.com>
2015-05-13 15:56:58 -0500

    Expanded on hello world with types.

--- 2015/typescript/helloworld.js
+++ 2015/typescript/helloworld.js
@@ -1,6 +1,10 @@
 function greeter(person) {
     return 'Hello, ' + person;
 }
+function add6(input) {
+    return input + 6;
+}
 var user = 'John User';
 var greeting = greeter(user);
-document.body.innerHTML = greeting;
+var age = add6(30);
+document.body.innerHTML = greeting + '. You will be ' + age + ' in six years.';
--- 2015/typescript/helloworld.ts
+++ 2015/typescript/helloworld.ts
@@ -1,9 +1,14 @@
-function greeter(person) {
+function greeter( person: string ) {
     return 'Hello, ' + person;
+}
+
+function add6( input: number ): number {
+    return input + 6;
 }
 
 var user = 'John User';
 
 var greeting = greeter( user );
+var age = add6( 30 );
 
-document.body.innerHTML = greeting;
+document.body.innerHTML = greeting + '. You will be ' + age + ' in six years.';
